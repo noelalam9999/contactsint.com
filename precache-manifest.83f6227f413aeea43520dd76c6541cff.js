fef4de3e6894b23a0c0a61f43b5268663efe4bd4
unknown <noel.alam9999@gmail.com>
2021-03-02 15:28:11 +0600

    Updates

---
+++ precache-manifest.83f6227f413aeea43520dd76c6541cff.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abd95cd3ab1db6c946b0123847c01bcd",
+    "url": "/contactsint.com/index.html"
+  },
+  {
+    "revision": "77b7a6534eb9a9504d22",
+    "url": "/contactsint.com/static/css/2.ff645aaf.chunk.css"
+  },
+  {
+    "revision": "e988a68d7271c5b69b95",
+    "url": "/contactsint.com/static/css/main.f312107a.chunk.css"
+  },
+  {
+    "revision": "77b7a6534eb9a9504d22",
+    "url": "/contactsint.com/static/js/2.40a015e8.chunk.js"
+  },
+  {
+    "revision": "ce15a0ffcfe8b91e4f219c1d7bb8c5ef",
+    "url": "/contactsint.com/static/js/2.40a015e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e988a68d7271c5b69b95",
+    "url": "/contactsint.com/static/js/main.f717e30d.chunk.js"
+  },
+  {
+    "revision": "e8dd6cf36b175b0220f7",
+    "url": "/contactsint.com/static/js/runtime-main.b47a611a.js"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/contactsint.com/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "0f9928d77bc67692e90527ec092fcbd0",
+    "url": "/contactsint.com/static/media/feature-tile-icon-01.0f9928d7.svg"
+  },
+  {
+    "revision": "bd39f30434175e7c8b1d1c74fa6569fd",
+    "url": "/contactsint.com/static/media/feature-tile-icon-02.bd39f304.svg"
+  },
+  {
+    "revision": "66f37ba5c7e4b6648f04883fa637e634",
+    "url": "/contactsint.com/static/media/feature-tile-icon-03.66f37ba5.svg"
+  },
+  {
+    "revision": "d9cb99ceb6a66250792ed78f5765325b",
+    "url": "/contactsint.com/static/media/features-split-image-01.d9cb99ce.png"
+  },
+  {
+    "revision": "3c569239f88160018f79029ba5dc227d",
+    "url": "/contactsint.com/static/media/features-split-image-02.3c569239.png"
+  },
+  {
+    "revision": "ea6949039d6554ebf2cf109f333f5811",
+    "url": "/contactsint.com/static/media/illustration-section-01.ea694903.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/contactsint.com/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "2810a88b1d6365b363caeabe678f1629",
+    "url": "/contactsint.com/static/media/logo.2810a88b.svg"
+  }
+]);
--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import productReducers from "./productReducers";
-import cartReducers from "./cartReducers";
-
-export default combineReducers({
-  products: productReducers,
-  cart: cartReducers,
-});
